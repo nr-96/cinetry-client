e12caed963d122dc13dd5828087dfe57ab09639d
naveen <hello.nr@icloud.com>
2022-10-21 14:54:50 +0530

    main layout - header & footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,20 +1,36 @@
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import { Content } from '../components';
+import styled from 'styled-components';
 
-const { Header } = Layout;
+const { Header, Footer } = Layout;
 
 function MainLayout() {
   return (
     <Layout>
       <Header className="header">
-        <div className="logo">Test...</div>
+        <StyledBrand className="logo">CINETRY</StyledBrand>
       </Header>
       <Content type="primary" minHeight="100vh">
         <Outlet />
       </Content>
+      <Footer>
+        <StyledFooter>CINETRY - 2022</StyledFooter>
+      </Footer>
     </Layout>
   );
 }
+
+const StyledBrand = styled.div`
+  padding: 0 10px;
+  color: #dc0d0d;
+  font-weight: 800;
+  font-size: 24px;
+`;
+
+const StyledFooter = styled.div`
+  text-align: center;
+  font-size: 12px;
+`;
 
 export default MainLayout;
